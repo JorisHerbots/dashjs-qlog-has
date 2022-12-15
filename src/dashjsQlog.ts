7ab32b1dc452a612076c71360ecf07bf5d13b8cd
Mike Vandersanden <mike.vandersanden@uhasselt.be>
2022-12-15 13:22:17 +0100

    add deactivation of dashjs log

--- src/dashjsQlog.ts
+++ src/dashjsQlog.ts
@@ -2,16 +2,19 @@ import * as dashjs from "dashjs";
 
 export class DashjsQlog {
     private mediaPlayer: dashjs.MediaPlayerClass;
+    public active: boolean;
 
     constructor(mediaPlayer: dashjs.MediaPlayerClass, mediaPlayerEvents: dashjs.MediaPlayerEvents) {
+        this.active = false;
         this.mediaPlayer = mediaPlayer;
         this.hook(mediaPlayerEvents);
     }
 
-    private dummy() {
+    private dummy(hookArguments: IArguments) {
+        if (!this.active) { return; }
         let dummy_string = "dummy hook"
-        for (let index = 0; index < arguments.length; index++) {
-            const argument = arguments[index];
+        for (let index = 0; index < hookArguments.length; index++) {
+            const argument = hookArguments[index];
             dummy_string += `\t${argument.type}`
             if (argument.message) {
                 dummy_string += `{${argument.message}}`
@@ -19,7 +22,7 @@ export class DashjsQlog {
         }
 
         console.log(dummy_string);
-        // console.log(dummy_string, arguments);
+        // console.log(dummy_string, hookArguments);
     }
 
     // NATIVE EVENTS
@@ -28,6 +31,7 @@ export class DashjsQlog {
     // DASH JS EVENTS
 
     private onBufferUpdate() {
+        if (!this.active) { return; }
         //TODO
         console.log('buffer update', arguments);
     }
@@ -52,13 +56,13 @@ export class DashjsQlog {
             const eventValue = mediaPlayerEvents[eventKey];
             if (eventValue in hooks) {
                 //@ts-expect-error
-                this.mediaPlayer.on(eventValue, hooks[eventValue]);
+                this.mediaPlayer.on(eventValue, (...hookArguments: any) => {hooks[eventValue](<IArguments>hookArguments)});
                 // console.log("hook found", eventKey)
             } else if (ignoredEvents.includes(eventValue)) {
                 // no hook placed
                 // console.log('ignored', eventKey);
             } else {
-                this.mediaPlayer.on(eventValue, this.dummy);
+                this.mediaPlayer.on(eventValue, (...hookArguments: any) => {this.dummy(<IArguments>hookArguments)});
                 // console.log('dummied', eventKey);
             }
         }
